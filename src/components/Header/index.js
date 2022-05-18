723104c9b4086d1c1f2777f39a0850c507f47e5e
alexarizona00 <82853759+alexarizona00@users.noreply.github.com>
2022-05-17 21:01:19 -0700

    fleshed out header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,16 @@
-export default Header
+import React from "react";
+import coverJpg from "../../assets/cover.jpg";
+
+function Header(props){
+
+    return (
+        <header className="">
+            <h1> Alex Albright</h1>
+            <img src={coverJpg} alt="abstract background"></img>
+            {props.children}
+        </header>
+    )
+}
+
+
+export default Header;
